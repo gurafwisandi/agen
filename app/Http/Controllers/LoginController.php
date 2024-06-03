1d07c6b6250c611364c568216e35517537fc00c4
Gepezet <gepezet91@gmail.com>
2024-06-03 09:29:24 +0700

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -92,49 +92,4 @@ class LoginController extends Controller
             // something went wrong
         }
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Login  $login
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Login $login)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Login  $login
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Login $login)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Login  $login
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Login $login)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Login  $login
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Login $login)
-    {
-        //
-    }
 }
